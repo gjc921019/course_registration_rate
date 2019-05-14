29eb81406efa5c091c5de631821e76308efd44d7
masonkm <kmason1379@gmail.com>
2019-05-14 16:20:01 -0400

    added ajax

--- database/routes/index.js
+++ database/routes/index.js
@@ -8,9 +8,12 @@ const express = require("express");
 const app = express();
 
 const constructorMethod = app => {
+	app.use("/ajax", express.Router().get("*",async(req,res) =>{
+	  res.status(200).send("Valid course IDs are 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16");
+	}));
 	app.use("/", loginRoutes);
-  	app.use("/students", studentRoutes);
-  	app.use("/courses", courseRoutes);
+  app.use("/students", studentRoutes);
+  app.use("/courses", courseRoutes);
 
   app.use("*", (req, res) => {
       res.redirect("/");
